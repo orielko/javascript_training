60e1c919791ab6ece6eae1c1e3fad7108823cf9b
ori_elkobi <orisip@gmail.com>
2021-05-30 17:45:40 +0300

    todo comments

--- lesson1/home exercises/more/ex3.js
+++ lesson1/home exercises/more/ex3.js
@@ -1,8 +1,8 @@
 "use strict";
 //we can't use conversion of + on this excersise because it converts null input to 0
 let number=prompt("Please enter a number");
-//TODO: is it enough to check if it's a number only this way?
-if(isNaN(number)){
+//Can use isFinite() function which is the opposite of isNaN()
+if(isNaN(number)){ 
     alert("You must enter a numeric value");
 }else if(number===''|| number===null){
     alert("You didn't enter any value");
--- lesson1/home exercises/more/ex4.js
+++ lesson1/home exercises/more/ex4.js
@@ -6,5 +6,4 @@ alert(`Result for multiplying strings is: ${mul}`);
 console.log(mul);
 //My guess was wrong, when it tries to convert the string variable the result for this conversion is NaN, therfore the result for multiply NaN's is NaN
 //Checking my assumptions:
-//TODO: Are my assumptions correct?
 console.log(num1,parseInt(num1),parseInt(num1)*parseInt(num1),NaN*NaN);
