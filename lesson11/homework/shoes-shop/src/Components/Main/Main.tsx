b2d1219741b556e53e376b56d3c0b7ebf72204d5
ori_elkobi <orisip@gmail.com>
2021-07-06 20:39:41 +0300

    lesson 11 homework shoes shop

---
+++ lesson11/homework/shoes-shop/src/Components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import "./Main.css";
+
+function Main(): JSX.Element {
+    return (
+        <div className="Main">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/3HjIljJd-o0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        </div>
+    );
+}
+
+export default Main;
